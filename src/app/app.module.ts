9a0f26ecdcee3894aee7d694db2b4341255014b3
Namito Yokota <namitoyokota@icloud.com>
2024-05-04 10:29:20 -0500

    Use ngx-code-snippet component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HIGHLIGHT_OPTIONS, HighlightModule, HighlightOptions } from 'ngx-highlightjs';
+import { CodeSnippetModule } from 'ngx-code-snippet';
 import { NotationRevealModule } from 'ngx-notation-reveal';
 import { AppComponent } from './app.component';
-import { SnippetComponent } from './snippet/snippet.component';
 
 @NgModule({
-    declarations: [AppComponent, SnippetComponent],
-    imports: [BrowserModule, BrowserAnimationsModule, NotationRevealModule, HighlightModule],
-    providers: [
-        {
-            provide: HIGHLIGHT_OPTIONS,
-            useValue: <HighlightOptions>{
-                coreLibraryLoader: () => import('highlight.js/lib/core'),
-                languages: {
-                    typescript: () => import('highlight.js/lib/languages/typescript'),
-                    xml: () => import('highlight.js/lib/languages/xml'),
-                    node: () => import('highlight.js/lib/languages/node-repl'),
-                },
-            },
-        },
-    ],
+    declarations: [AppComponent],
+    imports: [BrowserModule, BrowserAnimationsModule, NotationRevealModule, CodeSnippetModule],
+    providers: [],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
